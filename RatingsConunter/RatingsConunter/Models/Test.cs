9496293fafdf396c50087d3f9500392473b67904
Nayani Madhushani <Nayani.M@CreativeSoftware.com>
2018-02-04 21:56:43 -0800

    My initial commit

---
+++ RatingsConunter/RatingsConunter/Models/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatingsConunter.Models
+{
+    public  class Test
+    {
+        //public int Ratings { get; set; }
+
+        int Ratings = 10;
+
+        public int RatingWidth
+        {
+            get
+            {
+                int y = (143 * Ratings) / 100;
+                return y;
+            }
+        }
+    }
+}
